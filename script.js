65e6471c9db156cc317b01f30c712f1667f32dd1
Natcheyy <natchey@protonmail.com>
2025-09-26 14:53:45 -0500

    Added the first features. Function to create a grid of 16 x 16. I returned the squares I made in the DOM as a nodelist to iterate through. Then I made a function to iterate through the nodelist, on every square (node?) I add a event listener. If mouseenter event is true, then i change the background color to pink.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Function to create a grid of 16 x 16
+//Outer loop is rows, inner loop is columns
+//So, row 0, it will go down the column
+//For every index, create element in the DOM as a div
+//Give the div a class name of squareDiv
+//Append that div to the rowDiv for flex
+//Return the nodelist of the squares, so return every square
+
 function createGrid(){
     const gridContainer = document.querySelector('.gridContainer');
     const rows = 16;
@@ -14,7 +22,22 @@ function createGrid(){
         gridContainer.appendChild(rowDiv);
     }
 
-
+    return gridContainer.querySelectorAll('.squareDiv');
 }
 
-createGrid();
+//Function that takes in gridSquares, I used the createGrid function.
+//createGrid() returns all the squares.
+//For each square, add an event listener on the mouseenter event.
+//Uses a callback function. For every square it will check the mouseenter and assign the
+//background color of the square to pink.
+
+function hoverOverSquareFromGrid(gridSquares){
+    gridSquares.forEach(function(square){
+        square.addEventListener('mouseenter', function(e){
+            // console.log(e.currentTarget);
+            e.currentTarget.style.background = 'pink';
+        });
+    });
+}
+
+hoverOverSquareFromGrid(createGrid());
